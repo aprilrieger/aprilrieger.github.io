6c36045a4c99b756c45aee21e5c49d595aea5bcc
April Rieger <aprilrieger@Aprils-MacBook-Pro.local>
2023-12-17 16:43:41 -0800

    Updates

---
+++ component---src-pages-imprint-js-bdb5067ce7d17e458f8b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[683],{3598:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294),i=n(4525);function l(){return r.createElement(r.Fragment,null,r.createElement(i.pQ,{title:"Imprint",useTitleTemplate:!0,noIndex:!0}),r.createElement(i.T3,null,r.createElement(i.Ek,{sectionId:"imprint",heading:"Imprint"})))}}}]);
+//# sourceMappingURL=component---src-pages-imprint-js-bdb5067ce7d17e458f8b.js.map
